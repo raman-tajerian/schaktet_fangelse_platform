c05bd047e543593bba52771924c3015c491d8a65
Martin-Backlund <martinbacklund45@gmail.com>
2026-09-24 13:25:59 +0200

    Lade till konstrukor till Utsvulten class (ärver från Medfange)

--- schaktet_fangelse_platform/Utsvulten.cs
+++ schaktet_fangelse_platform/Utsvulten.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace schaktet_fangelse_platform
 {
-    internal class Utsvulten
+    internal class Utsvulten : Medfange
     {
+        public Utsvulten() : base("Utsvulten", hp: 20, attack: 5, xpBeloning: 10, guldMin: 5, guldMax: 10)
+        {
+           
+        }
     }
 }
