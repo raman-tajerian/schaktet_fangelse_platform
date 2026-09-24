7109c7893a7c2af544e1f3d3593271bee82d020d
Martin-Backlund <martinbacklund45@gmail.com>
2026-09-24 11:20:04 +0200

    Lade till relevanta kommentarer

--- schaktet_fangelse_platform/Medfange.cs
+++ schaktet_fangelse_platform/Medfange.cs
@@ -4,16 +4,19 @@ using System.Text;
 
 namespace schaktet_fangelse_platform
 {
+    //Abstract för att den endast ska kunna skapa konkreta typer; utsvulten, hamstraren och bödeln.
     internal abstract class Medfange
     {
+        //Public get, private set - utomstående ska kunna läsa värdena, men bara klassen ska kunna ändra dem.
         public string Namn { get; private set; }
         public int HP { get; private set; }
         public int Attack { get; private set; }
         public int XPBeloning { get; private set; }
-        public int GuldMin { get; private set; }
-        public int GuldMax { get; private set; }
+        public int GuldMin { get; private set; } //Intervall för guld drop inte ett fast belopp
+        public int GuldMax { get; private set; } //
 
 
+        //Protected konstruktor för att endast subklasserna ska kunna anropa konstruktorn
         protected Medfange(string namn, int hp, int attack, int xpBeloning, int guldMin, int guldMax)
         {
             Namn = namn;
