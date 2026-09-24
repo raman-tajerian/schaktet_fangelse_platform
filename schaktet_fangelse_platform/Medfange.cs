7db99a5631599864542edce49643dbb24e0b4bb7
Martin-Backlund <martinbacklund45@gmail.com>
2026-09-24 11:12:22 +0200

    Lade till properties för Medfange samt konstrukor, lade till TakeDamage metod.

--- schaktet_fangelse_platform/Medfange.cs
+++ schaktet_fangelse_platform/Medfange.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace schaktet_fangelse_platform
 {
-    internal class Medfange
+    internal abstract class Medfange
     {
+        public string Namn { get; private set; }
+        public int HP { get; private set; }
+        public int Attack { get; private set; }
+        public int XPBeloning { get; private set; }
+        public int GuldMin { get; private set; }
+        public int GuldMax { get; private set; }
+
+
+        protected Medfange(string namn, int hp, int attack, int xpBeloning, int guldMin, int guldMax)
+        {
+            Namn = namn;
+            HP = hp;
+            Attack = attack;
+            XPBeloning = xpBeloning;
+            GuldMin = guldMin;
+            GuldMax = guldMax;
+        }
+        public bool TakeDamage(int skada)
+        {
+            HP = HP - skada;
+            if (HP <= 0)
+                return true;
+            else
+                return false;
+        }
+
     }
 }
