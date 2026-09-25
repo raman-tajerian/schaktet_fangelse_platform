d9ef4049eedee7dd60c8b9255eea666a1862cb05
Raman Tajerian <ramantajerian@gmail.com>
2026-09-25 10:47:45 +0200

    Main program påbörjat, skapar spelare och lista av monstertyper

--- schaktet_fangelse_platform/Program.cs
+++ schaktet_fangelse_platform/Program.cs
@@ -1,5 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace schaktet_fangelse_platform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Här skapar vi spelaren
+            Console.WriteLine("Välkommen till schaktet!");
+            Console.Write("Skriv ditt namn: ");
+            string namn = Console.ReadLine();
+            Fange fange = new Fange(namn);
+
+            // Lista av monstertyper
+            List<Medfange> monsterTyper = new List<Medfange>
+            {
+                new Utsvulten(),
+                new Hamstraren(),
+                new Bodeln()
+            };
+
+            Random rnd = new Random();
+
+            // Huvudloop som körs till spelaren dör
 
 
 
-Console.WriteLine("Hello world");
+
+
+        }
+    }
+}
