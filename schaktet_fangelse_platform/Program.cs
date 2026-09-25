9bedb60e5d50c63c10868891ba137c082f6f4dd3
Martin-Backlund <martinbacklund45@gmail.com>
2026-09-25 14:59:55 +0200

    lade till if sats för val 3,  lade till skada för fange vid anfall,

--- schaktet_fangelse_platform/Program.cs
+++ schaktet_fangelse_platform/Program.cs
@@ -33,6 +33,7 @@ namespace schaktet_fangelse_platform
                 Console.WriteLine("2) Vila och Läka");
                 Console.WriteLine("3) Hoppa på plattform och åk ner");
                 int val = int.Parse(Console.ReadLine());
+                int Level = fange.Level;
 
                 if (val == 1)
                 {
@@ -54,6 +55,7 @@ namespace schaktet_fangelse_platform
 
                         bool monsterDog = false;
                         bool fangeDog = false;
+                        
 
                         if (val2 == 1) //Försvara
                         {
@@ -65,11 +67,13 @@ namespace schaktet_fangelse_platform
                         else if (val2 == 2) //Anfall
                         {
                             int skada = fange.Attack - monster.Forsvar;
-
+                            int skada2 = monster.Attack;
+                           
+                            fangeDog = fange.TakeDamage(skada2);
                             monsterDog = monster.TakeDamage(skada);
 
                         }
-                        else if (val2 == 3)
+                        else if (val2 == 3) //spring
                         {
                             int skada = rnd.Next(1, monster.Attack + 1);
                             
@@ -81,13 +85,16 @@ namespace schaktet_fangelse_platform
                             
                             int Guld = monster.TappaGuld();
 
+                            Console.WriteLine($"{monster.Namn} blev besegrad");
+
                             striden = false;
 
                         }
                         if (fangeDog)
                         {
-                            Console.WriteLine("GAME OVER");
+                            Console.WriteLine($"{fange.Namn} dog\nGAME OVER");
                             striden = false;
+                            return;
                         }
                     }
 
@@ -106,14 +113,20 @@ namespace schaktet_fangelse_platform
 
                 else if (val == 3)
                 {
-                    Console.WriteLine("Du hoppar på plattformen och åker djupare ner i schaktet");
-                    fange.GainXP(10);
-                    fange.NyDag();
-
+                    if (Level >= 5)
+                    {
+                        Console.WriteLine("Du hoppar på plattformen och åker djupare ner i schaktet");
+                        fange.GainXP(10);
+                        fange.NyDag();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{fange.Namn} måste vara minst level 5 för att åka ner i schaktet");
+                    }
                 }
             }
 
-            Console.WriteLine($"\nSpelet är slut! Du överlevde i {fange.Dagar} dagar och nådde level {fange.Level}");
+            Console.WriteLine($"\nSpelet är slut! Du överlevde i {fange.Dagar} dagar och nådde level {fange.Level}. Total XP: {fange.XP}");
 
         }
     }
